3b9ce0b71b9217f53ca970084dd1027af08ab56d
theonlytoriii <torieyo669@gmail.com>
2023-07-15 11:26:58 -0700

    puts to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main -it prints out output using put 
+ * main -it prints out output using put
  *
  * Return: always (0)
  *
  */
 int main(void)
-(
+{
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
-)
+}
 
 
