65fc8355d17eb9e137c288d8e18e2b7c941be209
theonlytoriii <torieyo669@gmail.com>
2023-07-15 07:56:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,13 @@
 /**
  * main -it prints out output using put 
  *
- * Return: always (0) 
+ * Return: always (0)
  *
  */
-int main(void) 
+int main(void)
 (
-        puts("\"Programming is like building a multilingual puzzle\n");return(0);
-) 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+)
+
+
