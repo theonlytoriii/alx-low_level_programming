8a074d7f5fd1e8fda615d7afcc0a033e8a9c6a6d
theonlytoriii <torieyo669@gmail.com>
2023-07-18 09:08:06 -0700

    Readme file created

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,14 @@
 
 /**
  * print_alphabet - Make the alphabet
+ *
  * Return: void
  */
 
 void print_alphabet(void)
-
 {
-	char c;
-
-	for (c = 'a'; c <= '2'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
 }
